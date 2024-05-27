411f08b0c6ea9cb80f4531f6665ae5b43f77728f
liarnose <liarnose@gmail.com>
2024-05-27 16:55:02 +0900

    fix example build config

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   language: 'ja',
-  theme: ['@liarnose/vivliostyle-theme-spellbook-5e'],
+  theme: [
+    '@fontsource/ibm-plex-mono',
+    '@fontsource/libre-baskerville',
+    '@fontsource/m-plus-1-code',
+    '@fontsource/material-icons',
+    '@fontsource/noto-sans-jp',
+    '@fontsource/noto-serif-jp',
+    '@fontsource/yakuhanjp',
+    '@fontsource/yakuhanmp',
+    '@vivliostyle/theme-base',
+    '@liarnose/vivliostyle-theme-spellbook-5e'],
   workspaceDir: '.vivliostyle',
   entry: ['example/default.md', 'example/copyright.md'],
   output: "book.pdf"
